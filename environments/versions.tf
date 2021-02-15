a5c4fea63541902425fed2b65382c8da931a9b1b
Rishav Dhar <19497993+rdhar@users.noreply.github.com>
2021-02-15 00:13:55 +0000

    Fixed formatting of tf files

--- environments/versions.tf
+++ environments/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 0.14.0"
   required_providers {
     newrelic = {
-      source = "newrelic/newrelic"
+      source  = "newrelic/newrelic"
       version = "~> 2.18.0"
     }
   }
--- modules/alerts/main.tf
+++ modules/alerts/main.tf
@@ -12,7 +12,7 @@ variable "var__policy_name" {
 }
 
 variable "var__channel_ids" {
-  type        = list
+  type        = list(any)
   description = "List of alert channel IDs"
 }
 
@@ -28,214 +28,214 @@ resource "newrelic_alert_policy" "policy__alert" {
 }
 
 resource "newrelic_nrql_alert_condition" "response_time__threshold" {
-  name = "${var.var__policy_name} response time exceeded threshold"
-  type = "static"
-  value_function = "single_value"
+  name                         = "${var.var__policy_name} response time exceeded threshold"
+  type                         = "static"
+  value_function               = "single_value"
   violation_time_limit_seconds = 36000
-  policy_id = newrelic_alert_policy.policy__alert.id
+  policy_id                    = newrelic_alert_policy.policy__alert.id
   nrql {
-    query = "${local.nrql__response_time} FACET appName WHERE ${var.var__nrql_filter}"
+    query             = "${local.nrql__response_time} FACET appName WHERE ${var.var__nrql_filter}"
     evaluation_offset = 3
   }
   critical {
-    operator = "above"
-    threshold = 3
+    operator              = "above"
+    threshold             = 3
     threshold_occurrences = "ALL"
-    threshold_duration = 900
+    threshold_duration    = 900
   }
   warning {
-    operator = "above"
-    threshold = 3
+    operator              = "above"
+    threshold             = 3
     threshold_occurrences = "ALL"
-    threshold_duration = 720
+    threshold_duration    = 720
   }
 }
 
 resource "newrelic_nrql_alert_condition" "response_time__baseline" {
-  name = "${var.var__policy_name} response time above baseline"
-  type = "baseline"
-  baseline_direction = "upper_only"
+  name                         = "${var.var__policy_name} response time above baseline"
+  type                         = "baseline"
+  baseline_direction           = "upper_only"
   violation_time_limit_seconds = 36000
-  policy_id = newrelic_alert_policy.policy__alert.id
+  policy_id                    = newrelic_alert_policy.policy__alert.id
   nrql {
-    query = "${local.nrql__response_time} FACET appName WHERE ${var.var__nrql_filter}"
+    query             = "${local.nrql__response_time} FACET appName WHERE ${var.var__nrql_filter}"
     evaluation_offset = 3
   }
   critical {
-    operator = "above"
-    threshold = 3
+    operator              = "above"
+    threshold             = 3
     threshold_occurrences = "ALL"
-    threshold_duration = 900
+    threshold_duration    = 900
   }
   warning {
-    operator = "above"
-    threshold = 3
+    operator              = "above"
+    threshold             = 3
     threshold_occurrences = "ALL"
-    threshold_duration = 720
+    threshold_duration    = 720
   }
 }
 
 resource "newrelic_nrql_alert_condition" "throughput__baseline" {
-  name = "${var.var__policy_name} throughput above baseline"
-  type = "baseline"
-  baseline_direction = "upper_only"
+  name                         = "${var.var__policy_name} throughput above baseline"
+  type                         = "baseline"
+  baseline_direction           = "upper_only"
   violation_time_limit_seconds = 36000
-  policy_id = newrelic_alert_policy.policy__alert.id
+  policy_id                    = newrelic_alert_policy.policy__alert.id
   nrql {
-    query = "${local.nrql__throughput} FACET appName WHERE ${var.var__nrql_filter}"
+    query             = "${local.nrql__throughput} FACET appName WHERE ${var.var__nrql_filter}"
     evaluation_offset = 3
   }
   critical {
-    operator = "above"
-    threshold = 3
+    operator              = "above"
+    threshold             = 3
     threshold_occurrences = "ALL"
-    threshold_duration = 900
+    threshold_duration    = 900
   }
   warning {
-    operator = "above"
-    threshold = 3
+    operator              = "above"
+    threshold             = 3
     threshold_occurrences = "ALL"
-    threshold_duration = 720
+    threshold_duration    = 720
   }
 }
 
 resource "newrelic_nrql_alert_condition" "throughput__signal_lost" {
-  name = "${var.var__policy_name} throughput signal lost"
-  type = "static"
-  value_function = "single_value"
+  name                         = "${var.var__policy_name} throughput signal lost"
+  type                         = "static"
+  value_function               = "single_value"
   violation_time_limit_seconds = 36000
-  policy_id = newrelic_alert_policy.policy__alert.id
+  policy_id                    = newrelic_alert_policy.policy__alert.id
   nrql {
-    query = "${local.nrql__throughput} FACET appName WHERE ${var.var__nrql_filter}"
+    query             = "${local.nrql__throughput} FACET appName WHERE ${var.var__nrql_filter}"
     evaluation_offset = 3
   }
   critical {
-    operator = "above"
-    threshold = 999999
+    operator              = "above"
+    threshold             = 999999
     threshold_occurrences = "AT_LEAST_ONCE"
-    threshold_duration = 900
+    threshold_duration    = 900
   }
-  expiration_duration = 14400
-  open_violation_on_expiration = true
+  expiration_duration            = 14400
+  open_violation_on_expiration   = true
   close_violations_on_expiration = false
 }
 
 resource "newrelic_nrql_alert_condition" "error_rate__threshold" {
-  name = "${var.var__policy_name} error rate exceeded threshold"
-  type = "static"
-  value_function = "single_value"
+  name                         = "${var.var__policy_name} error rate exceeded threshold"
+  type                         = "static"
+  value_function               = "single_value"
   violation_time_limit_seconds = 36000
-  policy_id = newrelic_alert_policy.policy__alert.id
+  policy_id                    = newrelic_alert_policy.policy__alert.id
   nrql {
-    query = "${local.nrql__error_rate} FACET appName WHERE ${var.var__nrql_filter}"
+    query             = "${local.nrql__error_rate} FACET appName WHERE ${var.var__nrql_filter}"
     evaluation_offset = 3
   }
   critical {
-    operator = "above"
-    threshold = 1
+    operator              = "above"
+    threshold             = 1
     threshold_occurrences = "ALL"
-    threshold_duration = 900
+    threshold_duration    = 900
   }
   warning {
-    operator = "above"
-    threshold = 1
+    operator              = "above"
+    threshold             = 1
     threshold_occurrences = "ALL"
-    threshold_duration = 720
+    threshold_duration    = 720
   }
 }
 
 resource "newrelic_nrql_alert_condition" "error_rate__baseline" {
-  name = "${var.var__policy_name} error rate above baseline"
-  type = "baseline"
-  baseline_direction = "upper_only"
+  name                         = "${var.var__policy_name} error rate above baseline"
+  type                         = "baseline"
+  baseline_direction           = "upper_only"
   violation_time_limit_seconds = 36000
-  policy_id = newrelic_alert_policy.policy__alert.id
+  policy_id                    = newrelic_alert_policy.policy__alert.id
   nrql {
-    query = "${local.nrql__error_rate} FACET appName WHERE ${var.var__nrql_filter}"
+    query             = "${local.nrql__error_rate} FACET appName WHERE ${var.var__nrql_filter}"
     evaluation_offset = 3
   }
   critical {
-    operator = "above"
-    threshold = 3
+    operator              = "above"
+    threshold             = 3
     threshold_occurrences = "ALL"
-    threshold_duration = 900
+    threshold_duration    = 900
   }
   warning {
-    operator = "above"
-    threshold = 3
+    operator              = "above"
+    threshold             = 3
     threshold_occurrences = "ALL"
-    threshold_duration = 720
+    threshold_duration    = 720
   }
 }
 
 resource "newrelic_nrql_alert_condition" "cpu_utilisation__threshold" {
-  name = "${var.var__policy_name} cpu utilisation exceeded threshold"
-  type = "static"
-  value_function = "single_value"
+  name                         = "${var.var__policy_name} cpu utilisation exceeded threshold"
+  type                         = "static"
+  value_function               = "single_value"
   violation_time_limit_seconds = 36000
-  policy_id = newrelic_alert_policy.policy__alert.id
+  policy_id                    = newrelic_alert_policy.policy__alert.id
   nrql {
-    query = "${local.nrql__cpu_utilisation} FACET appName WHERE ${var.var__nrql_filter}"
+    query             = "${local.nrql__cpu_utilisation} FACET appName WHERE ${var.var__nrql_filter}"
     evaluation_offset = 3
   }
   critical {
-    operator = "above"
-    threshold = 70
+    operator              = "above"
+    threshold             = 70
     threshold_occurrences = "ALL"
-    threshold_duration = 900
+    threshold_duration    = 900
   }
   warning {
-    operator = "above"
-    threshold = 70
+    operator              = "above"
+    threshold             = 70
     threshold_occurrences = "ALL"
-    threshold_duration = 720
+    threshold_duration    = 720
   }
 }
 
 resource "newrelic_nrql_alert_condition" "cpu_utilisation__baseline" {
-  name = "${var.var__policy_name} cpu utilisation above baseline"
-  type = "baseline"
-  baseline_direction = "upper_only"
+  name                         = "${var.var__policy_name} cpu utilisation above baseline"
+  type                         = "baseline"
+  baseline_direction           = "upper_only"
   violation_time_limit_seconds = 36000
-  policy_id = newrelic_alert_policy.policy__alert.id
+  policy_id                    = newrelic_alert_policy.policy__alert.id
   nrql {
-    query = "${local.nrql__cpu_utilisation} FACET appName WHERE ${var.var__nrql_filter}"
+    query             = "${local.nrql__cpu_utilisation} FACET appName WHERE ${var.var__nrql_filter}"
     evaluation_offset = 3
   }
   critical {
-    operator = "above"
-    threshold = 3
+    operator              = "above"
+    threshold             = 3
     threshold_occurrences = "ALL"
-    threshold_duration = 900
+    threshold_duration    = 900
   }
   warning {
-    operator = "above"
-    threshold = 3
+    operator              = "above"
+    threshold             = 3
     threshold_occurrences = "ALL"
-    threshold_duration = 720
+    threshold_duration    = 720
   }
 }
 
 resource "newrelic_nrql_alert_condition" "memory_usage__baseline" {
-  name = "${var.var__policy_name} memory usage above baseline"
-  type = "baseline"
-  baseline_direction = "upper_only"
+  name                         = "${var.var__policy_name} memory usage above baseline"
+  type                         = "baseline"
+  baseline_direction           = "upper_only"
   violation_time_limit_seconds = 36000
-  policy_id = newrelic_alert_policy.policy__alert.id
+  policy_id                    = newrelic_alert_policy.policy__alert.id
   nrql {
-    query = "${local.nrql__memory_usage} FACET appName WHERE ${var.var__nrql_filter}"
+    query             = "${local.nrql__memory_usage} FACET appName WHERE ${var.var__nrql_filter}"
     evaluation_offset = 3
   }
   critical {
-    operator = "above"
-    threshold = 3
+    operator              = "above"
+    threshold             = 3
     threshold_occurrences = "ALL"
-    threshold_duration = 900
+    threshold_duration    = 900
   }
   warning {
-    operator = "above"
-    threshold = 3
+    operator              = "above"
+    threshold             = 3
     threshold_occurrences = "ALL"
-    threshold_duration = 720
+    threshold_duration    = 720
   }
 }
